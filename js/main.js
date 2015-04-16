fbf33b310e6483f878380944094a9fb8f19554a5
wenfeng <elricfeng@gmail.com>
2015-04-16 19:21:00 +0200

    Add background music and sound effect
    1. Add sound effect when collecting stars and jumping
    2. Add background music
    3. Fix bugs of the score points

--- js/main.js
+++ js/main.js
@@ -1,35 +1,42 @@
-// var width = document.getElementById("game").innerWidth;
-// var width = window.innerWidth;
+// Width and height of the game canvas
 var width = 1200;
 var height = 500;
-
+// Game elements
 var runner = null;
-// var ledge = null;
 var stars = null;
 var obstacles = null;
-
-var v = -100;
-// var h = height - 100;
-
-var score = 0;
+// Scores
 var scoreText = null;
+var score = 0;
+// Sounds
+var background_music = document.getElementById("background_music");
+// background_music.stop();
+var jump_music;
+var star_collection_music;
+// Moving velocity
+var v = -200;
 
 
-// var game = new Phaser.Game(width, height, Phaser.AUTO, "game", { preload: preload, create: create, update: update }, true);
+
+
+// Game instance
 var game = new Phaser.Game(width, height, Phaser.AUTO, 'game', true);
 
-var PhaserGame = function () {
-    // var wasStanding = false;
-};
+var PhaserGame = function () { };
 
 PhaserGame.prototype = {
 
     preload: function () {
+        // Load images
         game.load.image('sky', 'assets/sky.png');
         game.load.image('ground', 'assets/platform.png');
         game.load.image('star', 'assets/star.png');
         game.load.spritesheet('dude', 'assets/dude.png', 32, 48); 
         game.load.image('obstacle', 'assets/obstacle.png'); 
+        // Load music
+        // game.load.audio('background_music', 'assets/background_music2.mp3');
+        game.load.audio('jump_music', 'assets/jump.mp3');
+        game.load.audio('star_collection_music', 'assets/collect_star.mp3');
     },
 
 
@@ -99,6 +106,33 @@ PhaserGame.prototype = {
         //  The score
         scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
+
+        /****** Game music *******/
+
+        // background_music = game.add.audio('background_music');
+        jump_music = game.add.audio('jump_music');
+        star_collection_music = game.add.audio('star_collection_music');
+
+        //  Being mp3 files these take time to decode, so we can't play them instantly
+        //  Using setDecodedCallback we can be notified when they're ALL ready for use.
+        //  The audio files could decode in ANY order, we can never be sure which it'll be.
+
+        // game.sound.setDecodedCallback([ jump_music, star_collection_music ], this.start_music, this);
+
+
+    },
+
+
+    mute_background_music: function () {
+        if (!background_music.muted){
+            background_music.muted = true;
+        } 
+    },
+
+    out_background_music: function () {
+        if (background_music.muted){
+            background_music.muted = false;
+        } 
     },
 
     setFriction: function (runner, platform) {
@@ -123,18 +157,21 @@ PhaserGame.prototype = {
         stars.forEach(this.freeze, this);
         runner.animations.stop();
         runner.frame = 4;
+        this.mute_background_music();
 
     },
 
     collectStar: function (runner, star) {
         
         // Removes the star from the screen
-        // star.kill();
         star.body.x = width / 4 * 5;
 
-        //  Add and update the score
+        // Add and update the score
         score += 10;
         scoreText.text = 'Score: ' + score;
+
+        // Play sound
+        star_collection_music.play();
 
     },
 
@@ -164,6 +201,8 @@ PhaserGame.prototype = {
 
 
     update: function () {
+        // Start background music
+
         //  Collide the runner and the stars with the platforms
         game.physics.arcade.collide(runner, platforms, this.setFriction, null, this);
         game.physics.arcade.collide(runner, obstacles, this.gameover, null, this);
@@ -177,13 +216,10 @@ PhaserGame.prototype = {
 
         // Jump if not yet
         var standing = runner.body.blocked.down || runner.body.touching.down;
-        // Set timer when runner hits the ground
-        // if (!standing && this.wasStanding) {
-        //     this.timer = game.time.time;
-        // }
 
         if (game.input.keyboard.isDown(Phaser.Keyboard.UP) && standing) {
             runner.body.velocity.y = -500;
+            jump_music.play();
         }
 
         // Show the jumping image
@@ -202,7 +238,9 @@ PhaserGame.prototype = {
         if (v === 0 && game.input.keyboard.isDown(Phaser.Keyboard.ENTER))
         {
             game.state.start(game.state.current);
-            v = -100;
+            v = -200;
+            score = 0;
+            this.out_background_music();
         }
     },
 
